0d8d5235e42f52e801b10ebe5feda21f19c4d9c4
jey <jey@umbraco.dk>
2024-03-06 10:59:37 +0100

    Added code to upload blob into Umbraco Cloud project's blob storage

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using Azure.Storage.Blobs;
+
+namespace AzureBlob;
+
+class Program
+{
+    public static BlobContainerClient getContainer(string connectionString, string containerName)
+    {
+        BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+        BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+        return containerClient;
+
+    }
+    
+    
+        
+    static async Task  Main(string[] args)
+    {
+
+
+        string SAS = "<Shared access signature URL (SAS) from Umbraco Cloud settings>";
+        string containerName = "<Container Name from the Umbraco Cloud settings>";
+
+
+      
+        string connectionString = $"BlobEndpoint={SAS}";
+        
+        
+
+        BlobContainerClient containerClient= getContainer(connectionString, containerName);
+        
+        
+        string localPath = "data";
+        Directory.CreateDirectory(localPath);
+        string fileName = Guid.NewGuid().ToString() + ".txt";
+        string localFilePath = Path.Combine(localPath, fileName);
+        
+        await File.WriteAllTextAsync(localFilePath, "Hello, World!"); 
+        
+        
+        // Get a reference to a blob
+        string blobName = "folder/" + Guid.NewGuid().ToString() + ".txt";
+        BlobClient blobClient = containerClient.GetBlobClient(blobName);
+
+        Console.WriteLine("Uploading to Blob storage as blob:\n\t {0}\n", blobClient.Uri);
+        
+        // Upload data from the local file, overwrite the blob if it already exists
+        await blobClient.UploadAsync(localFilePath, true);
+        
+    }
+}
